08b98b03042226254ece09255067e4f248f527b8
Orlando Buzana <orlandobuzana@outlook.com>
2019-08-12 20:37:15 -0400

    concept prob will be final

--- liri.js
+++ liri.js
@@ -9,46 +9,6 @@ let moment = require("moment");
 var keys = require("./keys.js");
 var spotify = new spotify(keys.spotify);
 
-
-/*
-// Then run a request with axios to the OMDB API with the movie specified
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log("The movie's rating is: " + response.data.imdbRating);
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
-
-
-   
-  spotify.search({ type: 'track', query: 'Silent in the morning' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-  console.log("======Artist Name=======")
-  console.log(data.tracks.items[0].album.artists[0]);
-  console.log(`Previrew : ${data.tracks.items[7].album.external_urls.spotify}`); 
-  }); */
-
-  //calling the name of the user
 inquirer
 .prompt([
   {
